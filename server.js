05f97fc223214a7168a85dd790cb1d189e07354a
mohragk <sandervermeer1@gmail.com>
2019-10-17 16:05:54 +0200

    sdfsd

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const server = app.listen(port);
 app.use(express.static('public'));
 
 console.log('Server started at port: ', port);
-console.log('TEST REDHAT');
 
 const socket = require('socket.io');
 const io = socket(server);
