cac733340bcaa4708aa04d922d899b07b9ccef70
mohragk <sandervermeer1@gmail.com>
2019-10-21 14:59:16 +0200

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const Simulator = require('./public/simulator.js');
 const express = require('express');
 const app = express();
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 const server = app.listen(port);
 app.use(express.static('public'));
 
